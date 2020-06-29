73c302a82090c71c48ef1f8a44962a53671d5f23
Brock <brock.reece@croud.co.uk>
2020-06-29 17:27:53 +0100

    Use state machine and build UI based on state

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="state.matches('loading')">Loading...</div>
+    <div v-else-if="state.matches('submitting')">Submitting...</div>
+    <div v-else-if="state.matches('submitted')">
+      Submitted: {{ form.emailAddress }}
+    </div>
+    <div v-else >
+      <input ref="email" type="email" v-model="form.emailAddress" placeholder="Email address">
+      <button @click="submit" :disabled="state.matches('ready') || state.matches('invalid')">Submit</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useMachine } from '@xstate/vue'
+import { formMachine } from './stateMachines/form'
 
 export default {
+  setup() {
+    const { state, send } = useMachine(formMachine, { devTools: true })
+    return {
+      state,
+      send
+    }
+  },
+
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      form: {
+        emailAddress: ''
+      }
+    }
+  },
+
+  computed: {
+    validForm() {
+      return !!(this.form.emailAddress && this.$refs.email.checkValidity())
+    }
+  },
+
+  watch: {
+    validForm(val) {
+      this.send(val ? 'VALID_INPUT' : 'INVALID_INPUT')
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.send('LOADED')
+    }, 500)
+  },
+
+  methods: {
+    submit() {
+      this.send('SUBMIT_FORM')
+      setTimeout(() => {
+        this.send('SUCCESS')
+      }, 500)
+    }
   }
 }
 </script>
